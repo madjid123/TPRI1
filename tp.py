9c91b87bcd5200ccf4e319925fc896eb96301637
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-24 19:02:48 +0100

    adding some serious shiii

--- tp.py
+++ tp.py
@@ -1,9 +1,10 @@
 from nltk import ngrams
-
-sentence = 'this is a foo bar sentences and i want to ngramize it'
-
-n = 6
-n_grams = ngrams(sentence.split(), n)
-
-for grams in n_grams:
-    print(grams)
+import nltk
+en_corps = """Hello my name is madjid and i would like to try ngrams with zipf law classification, 
+and i would like to know how much this will produce and how much this will be fun"""
+n = int(input("Enter the number of ngrams"))
+n_grams = ngrams(en_corps.split(), n)
+for ng in n_grams:
+    print(ng)
+    freq = nltk.FreqDist(ng)
+    print(freq.values())
