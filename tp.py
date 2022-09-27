a83f4160d6ca4d74ce43e25e6d0972ff94c6d6fd
Abdelmadjid Benimam <45493169+madjid123@users.noreply.github.com>
2022-09-27 18:20:12 +0100

    introducing plaintextcorpus + fixing utf json

--- tp.py
+++ tp.py
@@ -3,25 +3,27 @@ import nltk
 from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt
 import json
+import os
+from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 
 # types of params
 typeofclass = 2
 lang = "en"
 n = 2
-
-en_corpus = """
-"""
-# ar_corpus = """"""
+c_path = "./corpora"
+c_path = os.path.abspath(c_path)
 typeofclass = int(
     input("to use word based classification use 1, for character based one use 2: "))
 lang = input("enter en for english corpus, ar for the arabic one: ")
 
 if lang == "en":
-    corpus = en_corpus
+    c_path += "/english"
 else:
-    corpus = ar_corpus
+    c_path += "/arabic"
 
-corpus = word_tokenize(corpus)
+corpus = PlaintextCorpusReader(c_path, ".*")
+corpus = corpus.words()
+# corpus = word_tokenize(corpus)
 
 n = int(input("Enter the number of ngrams: "))
 if typeofclass == 2:
@@ -38,6 +40,6 @@ plt.xlabel('rank(f)', fontsize=14, fontweight='bold')
 plt.ylabel('frequency(r)', fontsize=14, fontweight='bold')
 plt.grid(True)
 plt.show()
-f = open("classement.txt", "a")
-f.write(json.dumps(ngram_fd.most_common()))
+f = open("classement.txt", "w", encoding="utf-8")
+f.write(json.dumps(ngram_fd.most_common(), ensure_ascii=False))
 f.close()
